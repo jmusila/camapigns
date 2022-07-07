ecfabe9e79f685a6273bc62581e33a589bc9d3db
jmusila <jonathanmusila6@gmail.com>
2022-07-07 11:03:35 +0300

    campaigns:list all campaigns

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CampaignResource;
+use App\Models\Campaign;
 use Illuminate\Http\Request;
 
 class CampaignController extends Controller
 {
-    //
+    public function index()
+    {
+        return CampaignResource::collection(Campaign::with('campaignImages')->get());
+    }
 }
