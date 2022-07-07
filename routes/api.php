7154c5d81185e95fae5ceb7305e850c52f9041d1
jmusila <jonathanmusila6@gmail.com>
2022-07-07 11:14:41 +0300

    campaigns:list all campaigns

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CampaignController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1/'], function () {
+    Route::group(['prefix' => 'campaigns'], function () {
+        Route::controller(CampaignController::class)->group(function () {
+            Route::get('list/all', 'index');
+        });
+    });
 });
