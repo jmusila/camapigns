9d48f3554b7487989d8af450ee31e12ce9e0a4fb
jmusila <jonathanmusila6@gmail.com>
2022-07-07 10:52:02 +0300

    all

---
+++ app/Http/Resources/CampaignResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CampaignResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'campaign_name' => $this->name,
+            'total_budget' => $this->total_budget,
+            'daily_budget' => $this->daily_budget,
+            'campaign_images' => new CampaignImagesResource($this->whenLoaded('campaignImages')),
+        ];
+    }
+}
