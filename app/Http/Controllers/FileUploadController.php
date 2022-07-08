3461710005d7b853dcf24d7c68cc3091f0607ed7
jmusila <jonathanmusila6@gmail.com>
2022-07-08 11:23:28 +0300

    ft:create file method

---
+++ app/Http/Controllers/FileUploadController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateFileRequest;
+use App\Http\Resources\FileResource;
+use App\Models\FileUpload;
+use Illuminate\Http\Request;
+
+class FileUploadController extends Controller
+{
+    public function __invoke(CreateFileRequest $request)
+    {
+        $file = new FileUpload($request->validated());
+
+        return new FileResource($file);
+    }
+}
