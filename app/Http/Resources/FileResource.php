27e492da832a4b9f5a643ae562b704d640c37745
jmusila <jonathanmusila6@gmail.com>
2022-07-08 11:14:18 +0300

    ft:create file resource

---
+++ app/Http/Resources/FileResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'file_name' => $this->file_name,
+            'file_link' => $this->file_link,
+            'file_size' => $this->file_size,
+            'file_type' => $this->file_type,
+        ];
+    }
+}
